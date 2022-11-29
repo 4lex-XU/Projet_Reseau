01e788486d4480e13df8c177593057194e14b704
HUANG-Stephanie <H810.Stephanie@gmail.com>
2022-11-30 00:38:48 +0100

    commit_INIT

--- IPv4.py
+++ IPv4.py
@@ -1,20 +1,21 @@
 def lectureIPv4(trame):
-    if(trame[28] != 4):
+    if(trame[28] != "4"):
         print("La version de la trame ne correspond pas a IPv4")
         return 
     IHL = trame[29]
-    if(IHL > 5):
+    option = False
+    if(IHL != "5"):
         print("Option(s) IP presente")
         option = True
     total_length = trame[32:36]
     TTL = trame[44:46]
     protocol = trame[46:48]
-    IPSrc = trame[48:56]
-    IPDest = trame[56:63]
+    IPSrc = trame[52:60]
+    IPDest = trame[60:68]
     if(option):
-        RR = trame[63:65]
+        RR = trame[68:70]
         if(RR == "07"):
             print("Option Record Route")
-    return (option, protocol, IPSrc, IPDest)
+    return (IHL, protocol, IPSrc, IPDest)
 
 
--- TCP.py
+++ TCP.py
@@ -2,11 +2,12 @@ def lectureTCP(trame):
     PortSrc = trame[0:4]
     PortDest = trame[4:8]
     SN = trame[8:16]
-    THL = trame[16]
-    if(THL > 5):
-        print("Option(s) IP presente")
-        option = True
-        data = trame[60:]
+    THL = trame[24]
+    if(THL != "5"):
+        print("Option(s) TCP presente")
+        index = int(THL,16)
+        fin_entete = index*8
+        data = trame[fin_entete:]
     else:
-        data = trame[20:]
+        data = trame[40:]
     return (PortSrc, PortDest, data)
