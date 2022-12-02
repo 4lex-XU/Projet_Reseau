3be78a8029863d14a094b1820439bdaf14da1fc1
HUANG-Stephanie <H810.Stephanie@gmail.com>
2022-12-02 22:59:08 +0100

    commit

--- Code/Lecteurs/IPv4.py
+++ Code/Lecteurs/IPv4.py
@@ -1,19 +1,15 @@
 def lectureIPv4(trame):
     if(trame[28] != "4"):
-        print("La version de la trame ne correspond pas a IPv4")
-        return 
+        #La version de la trame ne correspond pas a IPv4
+        return (0,0,0,0)
     IHL = trame[29]
     option = False
     if(IHL != "5"):
-        print("Option(s) IP presente")
+        #Option(s) IP presente
         option = True
     total_length = trame[32:36]
     TTL = trame[44:46]
     protocol = trame[46:48]
     IPSrc = trame[52:60]
     IPDest = trame[60:68]
-    if(option):
-        RR = trame[68:70]
-        if(RR == "07"):
-            print("Option Record Route")
     return (IHL, protocol, IPSrc, IPDest)
--- Code/Tests/main.py
+++ Code/Tests/main.py
@@ -7,18 +7,20 @@ from Lecteurs.trames import *
 from Convertisseurs.decimale import *
 
 # on obtient le tableau des différentes trames
-trames = diviseurDeTrame("../../Trames/tcp.txt")
+Trames = diviseurDeTrame("../../Trames/tcp.txt")
 
-for trame in trames :
+for trame in Trames :
     MACdst, MACsrc, type = lectureEthernet(trame)
-    #si type ip
+    #TEST IPv4
     if(type == "0800") :
         IHL, protocol, IPSrc, IPDest = lectureIPv4(trame)
-        if(protocol == "06") :
-            indice = int(IHL,16)
-            fin_ip = indice * 8
-            debut_tcp = 28 + fin_ip
-            PortSrc, PortDest, data = lectureTCP(trame[debut_tcp:])
-            print("Port Source : ", int(PortSrc,16))
-            print("Port Destinataire : ",int(PortDest,16))
-            print(data)
+        if(IHL != 0):
+            #TEST TCP
+            if(protocol == "06") :
+                indice = int(IHL,16)
+                fin_ip = indice * 8
+                debut_tcp = 28 + fin_ip
+                HTTP, PortSrc, PortDest, THL, FLAGS, Win, OPT, data = lectureTCP(trame[debut_tcp:])
+                print("Port Source : ", int(PortSrc,16))
+                print("Port Destinataire : ",int(PortDest,16))
+                print(data)
