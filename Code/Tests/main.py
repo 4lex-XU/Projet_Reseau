5516062d58d868ecc9adcf443be1b0f2eda888da
HUANG-Stephanie <H810.Stephanie@gmail.com>
2022-12-03 23:58:57 +0100

    commit_

--- Code/Tests/main.py
+++ Code/Tests/main.py
@@ -1,5 +1,6 @@
 import sys
 sys.path.append("..")
+
 from Lecteurs.Ethernet import *
 from Lecteurs.IPv4 import *
 from Lecteurs.TCP import *
@@ -9,6 +10,13 @@ from Convertisseurs.decimale import *
 
 # on obtient le tableau des différentes trames
 Trames = diviseurDeTrame("../../Trames/tcp.txt")
+
+#declaration des tableaux 
+Tab_PortSrc = []
+Tab_PortDest = []
+Tab_Comment = []
+
+# VARIABLES GLOBALES
 data_length = 0
 SN = 0
 AN = 0
@@ -17,39 +25,92 @@ for trame in Trames :
     MACdst, MACsrc, type = lectureEthernet(trame)
     #TEST IPv4
     if(type == "0800") :
-        IHL, protocol, IPSrc, IPDest = lectureIPv4(trame)
-        if(IHL != 0):
+        if(lectureIPv4(trame) != None):
+            IHL, protocol, IPSrc, IPDest = lectureIPv4(trame)
             #TEST TCP
             if(protocol == "06") :
                 indice = int(IHL,16)
                 fin_ip = indice * 8
                 debut_tcp = 28 + fin_ip
                 HTTP, PortSrc, PortDest, THL, FLAGS, Win, OPT, data = lectureTCP(trame[debut_tcp:])
-                Info = "" + int(PortSrc,16) + " -> " + int(PortDest,16)
-                flags = "["
-                if(FLAGS[0] == 1):
-                    flags += " URG"
-                if(FLAGS[1] == 1):
-                    flags += " ACK"
-                if(FLAGS[2] == 1):
-                    flags += " PSH"
-                if(FLAGS[3] == 1):
-                    flags += " RST"
-                if(FLAGS[4] == 1):
-                    flags += " SYN"
-                if(FLAGS[5] == 1):
-                    flags += " FIN"
-                flags += " ]"
-                Info += " " + flags
-                #SYN
-                if(FLAGS == (0,0,0,0,1,0)):
-                    Info += " SN = " + SN
-                if(FLAGS == (0,1,0,0,1,0)):
-                    Info += " SN = 0 AN = 1"
-                if(FLAGS == (0,1,0,0,0,0)):
-                    Info += "SN = 1 AN = 1" 
 
-                
-                
+                #AJOUT DANS LES TABLEAUX DE PORT SOURCE ET DESTINATION 
+                Tab_PortSrc.append((IPv4_dec(IPSrc), port_dec(PortSrc)))
+                Tab_PortDest.append((IPv4_dec(IPDest), port_dec(PortDest)))
 
-                print(data)
+                Comment = ""
+                #TEST HTTP
+                if(HTTP == -1):
+                    Comment = port_dec(PortSrc) + " -> " + port_dec(PortDest)
+                    flags = "["
+                    if(FLAGS[0] == "1"):
+                        flags += " URG"
+                    if(FLAGS[1] == "1"):
+                        flags += " ACK"
+                    if(FLAGS[2] == "1"):
+                        flags += " PSH"
+                    if(FLAGS[3] == "1"):
+                        flags += " RST"
+                    if(FLAGS[4] == "1"):
+                        flags += " SYN"
+                    if(FLAGS[5] == "1"):
+                        flags += " FIN"
+                    flags += " ]"
+                    Comment += " " + flags
+                    #TRAITEMENT DES FLAGS
+                    #SYN
+                    if(FLAGS == ("0","0","0","0","1","0")):
+                        SN = 0
+                        AN = 0
+                        data_length = 0
+                        Comment += " SN = 0"
+                    #SYN/ACK
+                    if(FLAGS == ("0","1","0","0","1","0")):
+                        AN = SN+1
+                        Comment += " SN = " + str(SN) + " AN = " + str(AN)
+                        SN += 1
+                    #ACK
+                    if(FLAGS == ("0","1","0","0","0","0")):
+                        tmp = SN
+                        SN = AN
+                        AN = tmp+data_length
+                        Comment += " SN = " + str(SN) + " AN = " + str(AN)
+                    #PUSH/ACK
+                    if(FLAGS == ("0","1","1","0","0","0")):
+                        SN = SN+data_length
+                        Comment += " SN = " + str(SN) + " AN = " + str(AN)
+                    
+                    data_length = len(data)//2
+
+                    #FIN
+                    if(FLAGS == ("0","0","0","0","0","1")):
+                        SN = SN+data_length
+                        Comment += " SN = " + str(SN) + " AN = " + str(AN)
+                        SN += 1
+                    #FIN/ACK
+                    if(FLAGS == ("0","1","0","0","0","1")):
+                        tmp = SN
+                        SN = AN
+                        AN = tmp
+                        Comment += " SN = " + str(SN) + " AN = " + str(AN)
+                        SN += 1
+                        
+                    Comment += " Win = " + str(Win) + " Data_length = " + str((len(data))//2) + " MSS = " + str(OPT[0])
+                    #SACK Permitted
+                    if(OPT[1] == 1):
+                        Comment += " SACK_PERM"
+                    Comment += " TSval = " + str(OPT[2]) + " TSecr = " + str(OPT[3]) + " WS = " + str(OPT[4])
+
+                elif(HTTP == 0): #REQUEST
+                    methode, contenu = lectureHTTPreq(trame[THL:])
+                    Comment = "HTTP: " + methode + " " + contenu
+                else: #REPLY
+                    version, code, message = lectureHTTPrep(trame[THL:])
+                    Comment = "HTTP: " + version + " " + code + " " + message
+                #AJOUT DU COMMENTAIRE DANS LE TABLEAU
+                #print(Comment)
+                Tab_Comment.append(Comment)
+
+#print(Tab_PortSrc)
+#print(Tab_PortDest)
+#print(Tab_Comment)
